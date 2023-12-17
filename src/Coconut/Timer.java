580a779d0a8461056502250d2c21931a7a4faf6c
Dylan <dylanjesse18@gmail.com>
2023-12-18 02:22:08 +1000

    added looping functionality to the Timer class

--- src/Coconut/Timer.java
+++ src/Coconut/Timer.java
@@ -3,15 +3,21 @@ package Coconut;
 public class Timer {
 	private long start = -1;
 	private long duration = 0;
+	private boolean loop;
 
 	public Timer() {}
 
-	public Timer(long duration) {
+	public Timer(long duration, boolean loop) {
 		this.duration = duration;
+		this.loop = loop;
 	}
 
 	public void setDuration(long duration) {
 		this.duration = duration;
+	}
+
+	public void setLoop(boolean loop) {
+		this.loop = loop;
 	}
 
 	public void start() {
@@ -19,8 +25,12 @@ public class Timer {
 	}
 
 	public boolean isExpired() {
-		if(start != -1)
-			return System.currentTimeMillis() - start > duration;
+		if(start != -1) {
+			boolean expired = System.currentTimeMillis() - start > duration;
+			if(loop && expired)
+				start += duration;
+			return expired;
+		}
 		else
 			return true;
 	}
